a0e6fcafcfb4a676fdcbc450cc7cd6de44c7ad29
jimlindybner <lin.jycl@gmail.com>
2023-03-18 19:35:57 -0400

    Add project files.

---
+++ SeasonApp/Controllers/SeasonAPIController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SeasonApp.Controllers
+{
+    public class SeasonAPIController : ApiController
+    {
+        /// <summary>
+        /// Receive input of temperature, provide message indicating season
+        /// </summary>
+        /// <param name="temperature">temperature in centigrade</param>
+        /// <example>
+        /// Get api/SeasonAPI/GetSeason/21 -> "summer"
+        /// </example>
+        /// <returns>
+        /// The associated season with the temperature
+        /// </returns>
+        [Route("api/SeasonAPI/GetSeason/{temperature}")]
+        public string GetSeason(int? temperature)
+        {
+            string season = "";
+            if (temperature == null)
+            {
+                return "unknown";
+            }
+
+            if (temperature > 20)
+            {
+                season = "summer";
+            }
+            else if (temperature >= 15)
+            {
+                season = "fall";
+            }
+            else if (temperature > 10)
+            {
+                season = "spring";
+            }
+            else
+            {
+                season = "winter";
+            }
+            //string message = "The season is " + season + "!";
+            return season;
+        }
+    }
+}
---
+++ SeasonApp/Controllers/SeasonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeasonApp.Controllers
+{
+    public class SeasonController : Controller
+    {
+        // GET: /Season/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /Season/Show
+        // Acquire info about season and send to show.cshtml
+        public ActionResult Show(int? temperature)
+        {
+            //Output string indicating temperature
+            if (temperature == null)
+            {
+                ViewBag.Temp = "unknown";
+            }
+            else
+            {
+                ViewBag.Temp = temperature.ToString();
+            }
+
+            //Output string message indicating season
+            SeasonAPIController controller = new SeasonAPIController();
+            ViewBag.Season = controller.GetSeason(temperature);
+
+            return View();
+        }
+    }
+}
